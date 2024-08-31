f7c0368d254110abeecb9ab9f7f01a230130ffe3
CiudadAlex <ciudad.alex@gmail.com>
2024-09-01 01:14:27 +0200

    TextAndSpeech

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from text2speech.Text2SpeechEngine import Text2SpeechEngine
 # FIXME https://www.geeksforgeeks.org/python-convert-speech-to-text-and-text-to-speech/
 
 
-text_2_speech_engine = Text2SpeechEngine("es")
+text_2_speech_engine = Text2SpeechEngine("en")
 text_2_speech_engine.say("Hi my name is tiki tiki slim shady")
 
 
