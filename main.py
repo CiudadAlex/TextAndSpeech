2b8abd52caabdd5e16885fa389d4d523e6a9329f
CiudadAlex <ciudad.alex@gmail.com>
2024-09-01 01:32:08 +0200

    TextAndSpeech

--- main.py
+++ main.py
@@ -2,8 +2,49 @@ from text2speech.Text2SpeechEngine import Text2SpeechEngine
 
 # FIXME https://www.geeksforgeeks.org/python-convert-speech-to-text-and-text-to-speech/
 
-
+'''
 text_2_speech_engine = Text2SpeechEngine("en")
 text_2_speech_engine.say("Hi my name is tiki tiki slim shady")
+'''
+
+import speech_recognition as sr
+
+# Initialize the recognizer
+r = sr.Recognizer()
 
 
+# Function to convert text to speech
+def SpeakText(command):
+    # Initialize the engine
+    text_2_speech_engine = Text2SpeechEngine("en")
+    text_2_speech_engine.say(command)
+
+
+# Loop infinitely for user to speak
+
+while True:
+
+    # Exception handling to handle exceptions at the runtime
+    try:
+
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+
+            # wait for a second to let the recognizer adjust the energy threshold based on the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+
+            # listens for the user's input
+            audio2 = r.listen(source2)
+
+            # Using google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            print("Did you say ", MyText)
+            SpeakText(MyText)
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("unknown error occurred")
