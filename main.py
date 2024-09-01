fd0410224ad976b9ce48f02bbcb3b4160700b7a0
CiudadAlex <ciudad.alex@gmail.com>
2024-09-01 02:41:56 +0200

    TextAndSpeech

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from text2speech.Text2SpeechEngine import Text2SpeechEngine
 from speech2text.Speech2TextListener import Speech2TextListener
 
-'''
-text_2_speech_engine = Text2SpeechEngine("en")
-text_2_speech_engine.say("Hi my name is tiki tiki slim shady")
-'''
+
+command = "Text2Speech"
 
 
 def speak_text(my_text):
     # Initialize the engine
     print("Did you say: ", my_text)
+    t2s = Text2SpeechEngine("en")
+    t2s.say(my_text)
+
+
+if "Text2Speech" == command:
     text_2_speech_engine = Text2SpeechEngine("en")
-    text_2_speech_engine.say(my_text)
+    text_2_speech_engine.say("Hi my name is tiki tiki slim shady")
 
 
-speech_2_text_listener = Speech2TextListener(speak_text)
-speech_2_text_listener.start()
+if "Speech2Text" == command:
+    speech_2_text_listener = Speech2TextListener(speak_text)
+    speech_2_text_listener.start()
+
+
+
+
